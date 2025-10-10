4f53f19581b242705a6568adfd06246e41f73550
gach1van <102760471+bigcodebingo@users.noreply.github.com>
2025-10-10 06:00:23 +0300

    (lab.1) setup database migrations

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,41 +1,25 @@
+// создается билдер веб приложения
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// зависимость, которая автоматически подхватывает все контроллеры в проекте
+builder.Services.AddControllers();
+// добавляем swagger
+builder.Services.AddSwaggerGen();
 
+// собираем билдер в приложение
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// добавляем 2 миддлвари для обработки запросов в сваггер
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+// добавляем миддлварю для роутинга в нужный контроллер
+app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// вместо *** должен быть путь к проекту Migrations
+// по сути в этот момент будет происходить накатка миграций на базу
+Migrations.Program.Main(Array.Empty<string>());
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
 
+// запускам приложение
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
